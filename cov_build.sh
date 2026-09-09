ed9b2baee9e5c2c78b6b830a519937eb0dd8bc98
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-09-09 17:24:35 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -1,13 +1,8 @@
 WORKDIR=`pwd`
-export ROOT=/usr
-export INSTALL_DIR=${ROOT}/local
-mkdir -p $INSTALL_DIR
 
+cd libchronyctl
 # Build libchronyctl (shared library + test_timectl CLI)
-cd $WORKDIR
 export INSTALL_DIR='/usr/local'
-export top_srcdir=`pwd`
-export top_builddir=`pwd`
-autoreconf -fiv
+autoreconf -i
 ./configure --prefix=${INSTALL_DIR}
 make && make install
